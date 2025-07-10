bab70701aa90589d0fab7e2024962b4958c6f588
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2025-07-10 09:29:52 -0400

    Update model.py

--- time_gan_tensorflow/model.py
+++ time_gan_tensorflow/model.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 
 from time_gan_tensorflow.utils import time_series_to_sequences, sequences_to_time_series
 from time_gan_tensorflow.modules import encoder_embedder, encoder, decoder, generator_embedder, generator, discriminator, simulator
-from time_gan_tensorflow.losses import binary_crossentropy, MeanSquaredError
-
+from time_gan_tensorflow.losses import binary_crossentropy, mean_squared_error
 class TimeGAN():
     def __init__(self,
                  x,
@@ -108,10 +107,10 @@ class TimeGAN():
                 p_hz = self.discriminator_model(hz)                              # log-odds of synthetic latent vector
 
                 # calculate the supervised loss
-                supervised_loss = MeanSquaredError(hx[:, 1:, :], hx_hat[:, :-1, :])
+                supervised_loss = mean_squared_error(hx[:, 1:, :], hx_hat[:, :-1, :])
                 
                 # calculate the autoencoder loss
-                autoencoder_loss = MeanSquaredError(x, x_hat) + \
+                autoencoder_loss = mean_squared_error(x, x_hat) + \
                                    self.lambda_param * supervised_loss
                                    
                 # calculate the generator loss
