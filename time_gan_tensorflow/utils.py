51386f04e1934b79c898a59407a12a908509c5f6
Pierce Roberts <134012559+PierceRoberts34@users.noreply.github.com>
2025-07-14 09:02:40 -0400

    Update utils.py

--- time_gan_tensorflow/utils.py
+++ time_gan_tensorflow/utils.py
@@ -2,10 +2,15 @@ import numpy as np
 
 def time_series_to_sequences(time_series, timesteps):
     '''
-    Reshape the time series as sequences.
+    Reshape the time series as overlapping sequences.
     '''
-    sequences = np.array([time_series[t - timesteps: t] for t in range(timesteps, len(time_series) + timesteps, timesteps)])
+    def time_series_to_sequences(time_series, timesteps):
+        sequences = np.array([
+            time_series[i: i + timesteps] 
+            for i in range(len(time_series) - timesteps + 1)
+        ])
     return sequences
+
 
 
 def sequences_to_time_series(sequences):
